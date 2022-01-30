58f97f1247c85212aef139bfca5ac103019cf41e
Stuart Wood <stuartwood2010@hotmail.com>
2022-01-29 18:57:06 -0700

    Working routes for html

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.listen(PORT, () => 
+    console.log(`listening on port ${PORT}`)
+);
