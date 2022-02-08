a9edc414d9638b41178a12c7287fdddca8afc423
Stuart Wood <stuartwood2010@hotmail.com>
2022-02-07 20:09:21 -0700

    Debugging heroku launch

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 // Import 'db.json' file
 let dbData = require('./db/db.json');
 const uuid = require('./helpers/uuid');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
